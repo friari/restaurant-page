29b8450974ccfd21d10b097ce2bef79f24dd6201
Ari <afriedgut1@gmail.com>
2023-11-23 12:30:02 +1100

    installed image-minimizer-webpack-plugin for image optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -32,6 +33,25 @@ module.exports = {
           'postcss-loader',
         ]
       }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      "...",
+      new ImageMinimizerPlugin({
+        minimizer: {
+          filename: "optimized-[name][ext]",
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              "imagemin-gifsicle",
+              "imagemin-jpegtran",
+              "imagemin-optipng",
+              "imagemin-svgo",
+            ]
+          }
+        }
+      })
     ]
   },
   output: {
